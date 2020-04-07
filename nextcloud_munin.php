0b08726600fd051d16e04a824a89c7573eb7a4bc
M.Gergő <gergo@bmrg.hu>
2020-04-08 01:35:30 +0200

    count Flashing users

--- nextcloud_munin.php
+++ nextcloud_munin.php
@@ -47,8 +47,8 @@ $time_req = time()-60*$set['diff_minutes'];
 		$ret['data']['users_web']['info'] = "Clients logged in by webpage.";
 		$ret['data']['users_app']['label'] = "By App";
 		$ret['data']['users_app']['info'] = "Connected clients with an app.";
-		$ret['data']['users_flash']['label'] = "Web(<5min)";
-		$ret['data']['users_flash']['info'] = "Rapid web users (succesfully logged out in less than 5 minutes)";
+		$ret['data']['users_flash']['label'] = "Web Flash";
+		$ret['data']['users_flash']['info'] = "Rapid web users (Successfully logged in, and out, in less than 5 minutes.)";
 
 
 		mysqli_set_charset($l, "utf8");
